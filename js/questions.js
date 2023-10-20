52d4b089240e78785231328c2b71bc6c254c989f
Adam Gilroy <adamgilroy22@gmail.com>
2023-10-20 12:04:15 +0000

    Add medium difficulty questions

--- js/questions.js
+++ js/questions.js
@@ -99,11 +99,110 @@ const questions = [
     points: 1,
    },
    {
-    question: 'Q',
+    question: "What is the name of the town where the events of Washington Irving's 'The Legend of Sleepy Hollow' take place?",
     answers : [
-        {text: '1', correct: false},
-        {text: '2', correct: true},
-        {text: '3', correct: false},
+        {text: 'Willowbrook', correct: false},
+        {text: 'Tarry Town', correct: true},
+        {text: 'Haunted Hollow', correct: false},
+    ],
+    points: 2,
+   },
+   {
+    question: 'Which 1993 Disney movie features three witches who return from the dead and cause havoc on Halloween night?',
+    answers : [
+        {text: 'The Witches of Eastwick', correct: false},
+        {text: 'The Nightmare Before Christmas', correct: true},
+        {text: 'Hocus Pocus', correct: true},
+    ],
+    points: 2,
+   },
+   {
+    question: 'Which classic monster is commonly associated with silver bullets as a means of defeating it?',
+    answers : [
+        {text: 'Mummy', correct: false},
+        {text: 'Werewolf', correct: true},
+        {text: 'Zombie', correct: false},
+    ],
+    points: 2,
+   },
+   {
+    question: 'In Mexican culture, what holiday is celebrated from October 31st to November 2nd to honor deceased loved ones?',
+    answers : [
+        {text: 'Day of the Dead', correct: true},
+        {text: 'Halloween', correct: false},
+        {text: "All Saint's Day", correct: false},
+    ],
+    points: 2,
+   },
+   {
+    question: 'Which famous magician and escape artists died on Halloween 1926?',
+    answers : [
+        {text: 'Copperfield', correct: false},
+        {text: 'Blaine', correct: false},
+        {text: 'Houdini', correct: true},
+    ],
+    points: 2,
+   },
+   {
+    question: 'Which classic horror novel features a scientist who creates a serum to transform into a monstrous, evil alter ego?',
+    answers : [
+        {text: 'Frankenstein', correct: false},
+        {text: 'Dr. Jekyll and Mr. Hyde', correct: true},
+        {text: 'Dracula', correct: false},
+    ],
+    points: 2,
+   },
+   {
+    question: 'What Halloween tradition involves creating a decorative arrangement of cornstalks, gourds, and pumpkins?',
+    answers : [
+        {text: 'Apple bobbing', correct: false},
+        {text: 'Pumpkin carving', correct: false},
+        {text: 'Harvest display', correct: true},
+    ],
+    points: 2,
+   },
+   {
+    question: "What is the name of the fictional hotel in Stephen King's 'The Shining' known for its supernatural occurrences?",
+    answers : [
+        {text: 'Overlook Hotel', correct: true},
+        {text: 'The Shady Rest Inn', correct: false},
+        {text: 'Bates Motel', correct: false},
+    ],
+    points: 2,
+   },
+   {
+    question: 'In the "Nightmare on Elm Street" film series, who is the villain who haunts teenagers in their dreams?',
+    answers : [
+        {text: 'Leatherface', correct: false},
+        {text: 'Freddy Krueger', correct: true},
+        {text: 'Michael Myers', correct: false},
+    ],
+    points: 2,
+   },
+   {
+    question: "In Bram Stoker's 'Dracula,' what is the primary weakness of vampires?",
+    answers : [
+        {text: 'Silver bullets', correct: false},
+        {text: 'Sunlight', correct: false},
+        {text: 'Garlic', correct: true},
+    ],
+    points: 2,
+   },
+   {
+    question: 'What is the name of the famous masked killer in the "Halloween" movie series?',
+    answers : [
+        {text: 'Jason Voorhees', correct: false},
+        {text: 'Michael Myers', correct: true},
+        {text: 'Freddy Kreuger', correct: false},
+    ],
+    points: 2,
+   },
+   {
+    question: 'What is the title of the 1973 horror film about a possessed young girl, directed by William Friedkin?',
+    answers : [
+        {text: 'The Exorcist', correct: true},
+        {text: "Rosemary's Baby", correct: false},
+        {text: 'Poltergeist', correct: false},
     ],
     points: 2,
    },

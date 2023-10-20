217b53ff1e87cceec76b28bc9b6f0d2f6b2e1db7
ShaneByrne0963 <>
2023-10-20 16:18:33 +0000

    Fix ribbon wallpaper to update on screen resize

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,9 +21,8 @@ window.onload = gameInit();
  * Is called when the page is loaded. Sets up the game
  */
 function gameInit() {
-    //randomizeWallpaper();
+    randomizeWallpaper();
     updateScreenSize();
-    setWallpaper('ribbon');
 }
 
 
@@ -72,6 +71,9 @@ function updateScreenSize() {
     // Adjusting the floor
     floor.style.height = `${cellSize / 8}px`;
     floor.style.backgroundSize = `${cellSize * 2}px`;
+
+    // Then updating the wallpaper to match the new grid
+    updateWallpaper(getWallpaper());
 }
 
 
@@ -93,6 +95,21 @@ function setWallpaper(wallpaperName) {
     let gameContainer = document.getElementById('game-container');
     gameContainer.style.backgroundImage = `url(./assets/images/game/wallpapers/wallpaper-${wallpaperName}.png)`;
 
+    updateWallpaper(wallpaperName);
+}
+
+
+/**
+ * Updates the properties of the wallpaper background
+ * @param {String} wallpaperName The name of the wallpaper that is set
+ */
+function updateWallpaper(wallpaperName) {
+    let gameContainer = document.getElementById('game-container');
+
+    // Clearing any previously set variables
+    gameContainer.style.backgroundRepeat = 'repeat';
+    gameContainer.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+
     // The ribbon wallpaper only repeats along the Y axis once and sits at the bottom of the wall
     if (wallpaperName === 'ribbon') {
         gameContainer.style.backgroundRepeat = 'repeat-x';
@@ -102,6 +119,24 @@ function setWallpaper(wallpaperName) {
         gridInfo = gridInfo.split(' ');
         let bottomCellPosition = parseFloat(gridInfo[0]) * (gridInfo.length - 2);
         gameContainer.style.backgroundPositionY = `${bottomCellPosition}px`;
-        console.log(bottomCellPosition);
     }
+}
+
+
+/**
+ * Finds the wallpaper that is currently decorating the room
+ * @returns {String} The found wallpaper
+ */
+function getWallpaper() {
+    let gameContainer = document.getElementById('game-container');
+    let wallpaperStyle = gameContainer.style.backgroundImage;
+
+    if (wallpaperStyle === '') {
+        return '';
+    }
+    // Removing the ".png)" part of the style property
+    wallpaperStyle = wallpaperStyle.slice(0, wallpaperStyle.length - 6);
+    // Then removing the "url(./assets/images/game/wallpapers/wallpaper-" part
+    wallpaperStyle = wallpaperStyle.slice(47);
+    return wallpaperStyle;
 }
